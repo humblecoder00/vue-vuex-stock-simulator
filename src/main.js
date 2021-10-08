eb54475bd809619d94ea0997effbabc65fbbd5ab
Şahin Arslan <sahin__arslan@hotmail.com>
2021-10-08 13:11:26 +0200

    progress

--- src/main.js
+++ src/main.js
@@ -1,23 +1,6 @@
-// // The Vue build version to load with the `import` command
-// // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-// import Vue from 'vue'
-// import App from './App'
-// import router from './router'
-
-// Vue.config.productionTip = false
-
-// /* eslint-disable no-new */
-// new Vue({
-//   el: '#app',
-//   router,
-//   components: { App },
-//   template: '<App/>'
-// })
-
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import axios from 'axios'
-import AxiosPlugin from 'vue-axios-cors'
 
 import App from './App.vue'
 // import { routes } from './routes'
@@ -27,7 +10,6 @@ import { store } from './store/store'
 Vue.config.productionTip = false
 
 Vue.use(VueRouter)
-Vue.use(AxiosPlugin)
 Vue.prototype.$http = axios
 
 const router = new VueRouter({
--- src/store/feature-stocks/services.js
+++ src/store/feature-stocks/services.js
@@ -1,10 +1,14 @@
 // handle api calls here
 import axiosInstance from '../../services/api-axios-instance'
 
+const URL = `${process.env.VUE_APP_API_URL}/data.json`
+
 // get data from firebase api
 export const getDataService = async () => {
+    // console.log('axios instance?', axiosInstance)
+    console.log('url?', URL)
     try {
-        const response = await axiosInstance.get('data.json')
+        const response = await axiosInstance.get(URL)
         return response.data
     } catch (err) {
         console.log(err)
@@ -14,7 +18,7 @@ export const getDataService = async () => {
 // saves data to firebase api
 export const saveDataService = async (data) => {
     try {
-        await axiosInstance.put('data.json', data)
+        await axiosInstance.put(URL, data)
     } catch (err) {
         console.log('error on save:', err)
     }
