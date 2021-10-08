92456d46ff5f1c6f9b5e557b3342f4dbfc1e2328
Şahin Arslan <sahin__arslan@hotmail.com>
2021-10-08 13:28:13 +0200

    progress

--- src/services/api-axios-instance.js
+++ src/services/api-axios-instance.js
@@ -1,9 +1,9 @@
-import axios from 'axios'
+// import axios from 'axios'
 
-const axiosInstance = axios.create({
-    baseURL: process.env.VUE_APP_API_URL,
-    // timeout: 1000,
-    // headers: { 'X-Custom-Header': 'foobar' },
-})
+// const axiosInstance = axios.create({
+//     baseURL: process.env.VUE_APP_API_URL,
+//     // timeout: 1000,
+//     // headers: { 'X-Custom-Header': 'foobar' },
+// })
 
-export default axiosInstance
+// export default axiosInstance
--- src/store/feature-stocks/services.js
+++ src/store/feature-stocks/services.js
@@ -1,10 +1,14 @@
+import axios from 'axios'
+
 // handle api calls here
-import axiosInstance from '../../services/api-axios-instance'
+// import axiosInstance from '../../services/api-axios-instance'
+
+axios.defaults.baseURL = process.env.VUE_APP_API_URL
 
 // get data from firebase api
 export const getDataService = async () => {
     try {
-        const response = await axiosInstance.get('data.json')
+        const response = await axios.get('data.json')
         return response.data
     } catch (err) {
         console.log(err)
@@ -14,7 +18,7 @@ export const getDataService = async () => {
 // saves data to firebase api
 export const saveDataService = async (data) => {
     try {
-        await axiosInstance.put('data.json', data)
+        await axios.put('data.json', data)
     } catch (err) {
         console.log('error on save:', err)
     }
